bdb897540737240c86aaa0d507e7280fd47b363d
Michelle Lavryssen <1010199@student.sae.edu.au>
2020-04-10 01:23:12 +1000

    Camera- Fixed Mouse Rotation (Michelle)
    Clamped Camera Zoom, Adjusted camera movement speed based on how zoomed in you are.

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -6,7 +6,9 @@ public class CameraController : MonoBehaviour
 {
     public Transform cameraTransform;
 
-    public float movementSpeed;
+    private float movementSpeed;
+    public float movementSpeedNormal;
+    public float movementSpeedFast;
     public float movementTime;
     public float rotationAmount;
     public Vector3 zoomAmount;
@@ -25,20 +27,48 @@ public class CameraController : MonoBehaviour
         newPosition = transform.position;
         newRotation = transform.rotation;
         newZoom = cameraTransform.localPosition;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (newZoom.y >=325)
+        {
+            movementSpeed = movementSpeedFast;
+        }
+        else
+        {
+            movementSpeed = movementSpeedNormal;
+        }
         HandleMouseInput();
         HandleMovementInput();
+       
     }
 
     void HandleMouseInput()
     {
         if (Input.mouseScrollDelta.y != 0)
         {
-            newZoom += Input.mouseScrollDelta.y * zoomAmount;
+            //multiply the zoom amount by 2 to make zooming on the mouse feel faster
+            newZoom += Input.mouseScrollDelta.y * (zoomAmount *2);     
+            if (newZoom.y < 50)
+            {
+                newZoom.y = 50;
+            }
+            if (newZoom.z > -50)
+            {
+                newZoom.z = -50;
+            }
+            if (newZoom.y > 700)
+            {
+                newZoom.y = 700;
+            }
+            if (newZoom.z < -700)
+            {
+                newZoom.z = -700;
+            }
+
         }
         if (Input.GetMouseButtonDown(0))
         {
@@ -68,15 +98,16 @@ public class CameraController : MonoBehaviour
         {
             rotateStartPosition = Input.mousePosition;
         }
-        if (Input.GetMouseButtonDown(2))
+        if (Input.GetMouseButton(2))
         {
             rotateCurrentPosition = Input.mousePosition;
             Vector3 difference = rotateStartPosition - rotateCurrentPosition;
-            
+
             //reset the drag position for the next frame
             rotateStartPosition = rotateCurrentPosition;
 
             //Negating the value here so that the world spins in the opposite direction of the drag 
+
             newRotation *= Quaternion.Euler(Vector3.up * (-difference.x / 5));
         }
     }
@@ -87,6 +118,7 @@ public class CameraController : MonoBehaviour
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             newPosition += (transform.forward * movementSpeed);
+
         }
 
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
@@ -113,11 +145,48 @@ public class CameraController : MonoBehaviour
         if (Input.GetKey(KeyCode.R) || Input.GetKey(KeyCode.LeftBracket))
         {
             newZoom += zoomAmount;
+            if (newZoom.y < 50)
+            {
+                newZoom.y = 50;
+            }
+            if (newZoom.z > -50)
+            {
+                newZoom.z = -50;
+            }
+            if (newZoom.y > 700)
+            {
+                newZoom.y = 700;
+            }
+            if (newZoom.z < -700)
+            {
+                newZoom.z = -700;
+            }
+
         }
         if (Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.RightBracket))
         {
             newZoom -= zoomAmount;
+
+            if (newZoom.y < 50)
+            {
+              newZoom.y = 50;
+            }
+            if (newZoom.z > -50)
+            {
+                newZoom.z = -50;
+            }
+            if (newZoom.y >700)
+            {
+                newZoom.y = 700;
+            }
+            if (newZoom.z < -700)
+            {
+                newZoom.z = -700;
+            }
+
+           
         }
+
 
 
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
